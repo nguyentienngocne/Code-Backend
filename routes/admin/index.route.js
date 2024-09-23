1b6a1c150cec6532b0ac62a992de26908f5e81cf
Nguyễn Tiến Ngọc <ntn322003@gmail.com>
2024-09-23 14:46:14 +0700

    Làm giao diện danh sách sản phẩm

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,10 @@
 const systemConfig = require("../../config/system")
 const dashboardRoute = require("./dashboard.route")
+const productRoute = require("./product.route")
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin
+
+  app.use(PATH_ADMIN + "/products",productRoute)
   app.use(PATH_ADMIN + "/dashboard",dashboardRoute); 
 }
