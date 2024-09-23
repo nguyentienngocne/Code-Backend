91c55f28c4367125768357213a9c3f054f6c1829
Nguyễn Tiến Ngọc <ntn322003@gmail.com>
2024-09-23 16:59:10 +0700

    Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -5,14 +5,47 @@ const Product = require("../../models/product.model");
 
 
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+
+  let filerStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status:"inactive",
+      class: ""
+    }
+  ]
+
+  if(req.query.status) {
+    let index = filerStatus.findIndex(item => item.status == req.query.status)
+    filerStatus[index].class = 'active';
+  }else {
+    const index = filerStatus.findIndex(item => item.status == "")
+    filerStatus[index].class = 'active';
+  }
+
+  let find = {
     deleted: false
+  }
 
-  })
 
-  console.log(products)
+
+  if(req.query.status) {
+    find.status = req.query.status
+  }
+  const products = await Product.find(find)
+
   res.render("admin/pages/products/index",{
     pageTitle: "Trang danh sách sản phẩm",
-    products: products
+    products: products,
+    fillerStatus: filerStatus
   })
 }
