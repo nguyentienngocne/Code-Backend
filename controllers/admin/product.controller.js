50ab08a5a7a78de22d0de19cef12e04c9feb5856
Nguyễn Tiến Ngọc <ntn322003@gmail.com>
2024-09-24 14:25:33 +0700

    Tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -41,11 +41,20 @@ module.exports.index = async (req, res) => {
   if(req.query.status) {
     find.status = req.query.status
   }
+
+  let keyword = "";
+  if(req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword,'i')
+    find.title = regex;
+  }
+
   const products = await Product.find(find)
 
   res.render("admin/pages/products/index",{
     pageTitle: "Trang danh sách sản phẩm",
     products: products,
-    fillerStatus: filerStatus
+    fillerStatus: filerStatus,
+    keyword: keyword
   })
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -17,3 +17,25 @@ if(buttonStatus.length > 0) {
     })
   })
 }
+
+// End Button Status
+
+
+// Form Search
+
+const formSearch = document.getElementById("form-search");
+if(formSearch) {
+  let url = new URL(window.location.href)
+
+  formSearch.addEventListener("submit",(e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value
+    if(keyword) {
+      url.searchParams.set("keyword",keyword)
+    }else {
+      url.searchParams.delete("keyword")
+    }
+
+    window.location.href = url.href
+  })
+}
