3e5d328822a139c4e132bae928dd6c3a2360edb2
Nguyễn Tiến Ngọc <ntn322003@gmail.com>
2024-09-27 07:42:52 +0700

    Tối ưu phần tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 
 const fillerStatusHelpers = require("../../helpers/fillerStatus")
+const searchStatusHelpers = require("../../helpers/search")
 // [GET] /admin/products
 
 
@@ -18,12 +19,13 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status
   }
 
-  let keyword = "";
-  if(req.query.keyword) {
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword,'i')
-    find.title = regex;
+  const objectSearch = searchStatusHelpers(req.query);
+  if(objectSearch.regex) {
+    find.title = objectSearch.regex
   }
+
+
+
 
   const products = await Product.find(find)
 
@@ -31,6 +33,6 @@ module.exports.index = async (req, res) => {
     pageTitle: "Trang danh sách sản phẩm",
     products: products,
     fillerStatus: filerStatus,
-    keyword: keyword
+    keyword: objectSearch.keyword
   })
 }
